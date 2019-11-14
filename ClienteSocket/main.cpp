b2410cb0ad29d8755c526fd5a098d2bc24d8cbc3
giannativo <giannativo@yahoo.com.ar>
2019-11-14 20:34:10 -0300

    envio de archivo funcionando sin bidireccionalidad

--- ClienteSocket/main.cpp
+++ ClienteSocket/main.cpp
@@ -55,54 +55,121 @@ public:
         string substring=var.substr(0,3);
 
         if(substring=="(M)" || substring=="(m)" ){
-        send(server, buffer, sizeof(buffer), 0);
-        cout << "Mensaje enviado!" << endl;
+            send(server, buffer, sizeof(buffer), 0);
+            cout << "Mensaje enviado!" << endl;
+            memset(buffer, 0, sizeof(buffer));
+        }
+        else if(substring=="(T)"|| substring=="(t)" ){
+            string ruta=var.substr(3);
+            FILE *File;
+            char *Buffer;
+            unsigned long Size;
+            char nombre[512]= "\0";
+            char rutaChar[1024] = "\0";
+            string mensaje = "(T)Enviando archivo...";
+            cout<<mensaje<<endl;
+            send(server, buffer, sizeof(buffer), 0);
 
-        }else if(substring=="(T)"|| substring=="(t)" ){
+            strcpy(nombre, &ruta[0]);
+            SafeSend(this->server, nombre, sizeof(nombre));
+            strcpy(rutaChar, &ruta[0]);
+            File = fopen(rutaChar, "rb");
 
-        string nombre = "C:/Users/Gian/Documents/example.txt";
-        ifstream file(nombre.c_str());
-        printf( "Fichero: %s -> ", nombre );
-            if( file ){
-            printf( "creado (ABIERTO)\n" );
-            ifstream file(nombre.c_str());
-            file.seekg( 0, ios::end );
-            size_t len = file.tellg();
-            char *ret = new char[len];
-            file.seekg(0, ios::beg);
-            file.read(ret, len);
-            file.close();
-            send(server, ret, sizeof(ret), 0);
+            if(!File){
+                cout<<"ERROR AL LEER EL ARCHIVO"<<endl;
+                cin.ignore();
             }
-            else
-            {
-              printf( "Error (NO ABIERTO)\n" );
+            else{
+                fseek(File, 0, SEEK_END);
+                Size = ftell(File);
+                fseek(File, 0, SEEK_SET);
+                Buffer = new char[Size];
+                fread(Buffer, Size, 1, File);
+                char cSize[MAX_PATH];
+                sprintf(cSize, "%lu", Size);
+                fclose(File);
+                Sleep(1000);
+                SafeSend(this->server, cSize, MAX_PATH);
+                Sleep(1000);
+                SafeSend(this->server,Buffer,Size);
             }
-
-        }else if(substring=="(F)"|| substring=="(f)" ){
-        send(server, buffer, sizeof(buffer), 0);
-        cout << "Mensaje enviado!" << endl;
-        }else{
-        cout << "Error de comando!" << endl;
-        memset(buffer, 0, sizeof(buffer));
-        Enviar();
+            free(Buffer);
+        }
+        else if(substring=="(F)"|| substring=="(f)" ){
+            send(server, buffer, sizeof(buffer), 0);
+            cout << "Mensaje enviado!" << endl;
+        }
+        else{
+            cout << "Error de comando!" << endl;
+            memset(buffer, 0, sizeof(buffer));
+            Enviar();
         }
         memset(buffer, 0, sizeof(buffer));
     }
 
     void Recibir()
     {
-         recv(server, buffer, sizeof(buffer), 0);
+        recv(server, buffer, sizeof(buffer), 0);
 
-         string varRespuesta(buffer);
-         string substringRespuesta=varRespuesta.substr(0,3);
-         string mensaje=varRespuesta.substr(3);
-         cout << "El servidor dice: " << mensaje << endl;
+        string varRespuesta(buffer);
+        string substringRespuesta=varRespuesta.substr(0,3);
+        string mensaje=varRespuesta.substr(3);
+        if(substringRespuesta=="(T)"|| substringRespuesta=="(t)"){
+            cout<<"Recibiendo archivo..."<<endl;
+            int Size;
+            char Filesize[MAX_PATH] = "\0";
+            char nombre[512] = "\0";
 
-         if(substringRespuesta=="(F)"|| substringRespuesta=="(f)"){
+            recv(this->server, nombre, sizeof(nombre), 0);
+
+            if(recv(this->server, Filesize, sizeof(Filesize), 0)){
+                Size = atoi((const char*)Filesize);
+                cout<< "Longitud del archivo: "<< Size<<endl;
+            }
+
+            char *Buffer = new char[Size];
+            recv(this->server, Buffer, Size, 0);
+            cout<< "Se trajo el archivo: "<< nombre <<endl;
+            char new_name[50];
+
+            cout<<"Ingresar nombre y extension del archivo a guardar: "<<endl;
+            scanf("%49s",new_name);
+
+            FILE* File;
+            File = fopen(new_name, "wb");
+            fwrite(Buffer, 1, Size, File);
+            fclose(File);
+
+            cout<<"Archivo recibido!"<<endl;
+            free(Buffer);
+        }
+        if(substringRespuesta=="(F)"|| substringRespuesta=="(f)"){
             CerrarSocket();
-         }
+        }
+        cout << "El servidor dice: " << mensaje << endl;
+
          memset(buffer, 0, sizeof(buffer));
+    }
+
+    int SafeSend(SOCKET s, char* buf, int buflen)
+    {
+	int sendlen = 0;
+	int totalsend = 0;
+	int remaining = buflen;
+
+	while(sendlen != buflen)
+	{
+		sendlen = send(s, &buf[totalsend], remaining, 0);
+
+		if(sendlen == SOCKET_ERROR)
+		{
+			return SOCKET_ERROR;
+		}
+
+		totalsend = totalsend + sendlen;
+		remaining = sendlen - totalsend;
+	}
+    return 0;
     }
 
     void CerrarSocket()
--- ServerSocket/main.cpp
+++ ServerSocket/main.cpp
@@ -43,18 +43,50 @@ public:
 
     void Recibir()
     {
+        recv(client, buffer, sizeof(buffer), 0);
 
-       recv(client, buffer, sizeof(buffer), 0);
+        string varRespuesta(buffer);
+        string substringRespuesta=varRespuesta.substr(0,3);
+        string mensaje=varRespuesta.substr(3);
+        if(substringRespuesta=="(M)"|| substringRespuesta=="(m)"){
+            cout << "El cliente dice: " << mensaje << endl;
+        }
+        else if(substringRespuesta=="(F)"|| substringRespuesta=="(f)"){
+            CerrarSocket();
+        }
+        else if(substringRespuesta=="(T)"|| substringRespuesta=="(t)"){
+            cout<<"Recibiendo archivo..."<<endl;
+            int Size;
+            char Filesize[MAX_PATH] = "\0";
+            char nombre[512] = "\0";
 
-       string varRespuesta(buffer);
-       string substringRespuesta=varRespuesta.substr(0,3);
-       string mensaje=varRespuesta.substr(3);
-       cout << "El cliente dice: " << mensaje << endl;
+            recv(this->client, nombre, sizeof(nombre), 0);
 
-            if(substringRespuesta=="(F)"|| substringRespuesta=="(f)"){
-                CerrarSocket();
+            if(recv(this->client, Filesize, sizeof(Filesize), 0)){
+                Size = atoi((const char*)Filesize);
+                cout<< "Longitud del archivo: "<< Size<<endl;
             }
-      memset(buffer, 0, sizeof(buffer));
+
+            char *Buffer = new char[Size];
+            recv(this->client, Buffer, Size, 0);
+            cout<< "Se trajo el archivo: "<< nombre <<endl;
+            char new_name[50];
+
+            cout<<"Ingresar nombre y extension del archivo a guardar: "<<endl;
+            scanf("%49s",new_name);
+
+            FILE* File;
+            File = fopen(new_name, "wb");
+            fwrite(Buffer, 1, Size, File);
+            fclose(File);
+
+            cout<<"Archivo recibido!"<<endl;
+            free(Buffer);
+        }
+        else{
+            CerrarSocket();
+        }
+        memset(buffer, 0, sizeof(buffer));
     }
     void Enviar()
     {
@@ -69,18 +101,75 @@ public:
         send(client, buffer, sizeof(buffer), 0);
         cout << "Mensaje enviado!" << endl;
 
-        }else if(substring=="(T)"|| substring=="(t)" ){
+        }
+        else if(substring=="(T)"|| substring=="(t)" ){
+            string ruta=var.substr(3);
+            FILE *File;
+            char *Buffer;
+            unsigned long Size;
+            char nombre[512]= "\0";
+            char rutaChar[1024] = "\0";
+            string mensaje = "(T)Enviando archivo...";
+            cout<<mensaje<<endl;
+            send(server, buffer, sizeof(buffer), 0);
 
-        }else if(substring=="(F)"|| substring=="(f)" ){
-        send(client, buffer, sizeof(buffer), 0);
-        cout << "Mensaje enviado!" << endl;
-        }else{
-        cout << "Error de comando!" << endl;
-        memset(buffer, 0, sizeof(buffer));
-        Enviar();
+            strcpy(nombre, &ruta[0]);
+            SafeSend(this->server, nombre, sizeof(nombre));
+            strcpy(rutaChar, &ruta[0]);
+            File = fopen(rutaChar, "rb");
+
+            if(!File){
+                cout<<"ERROR AL LEER EL ARCHIVO"<<endl;
+                cin.ignore();
+            }
+            else{
+                fseek(File, 0, SEEK_END);
+                Size = ftell(File);
+                fseek(File, 0, SEEK_SET);
+                Buffer = new char[Size];
+                fread(Buffer, Size, 1, File);
+                char cSize[MAX_PATH];
+                sprintf(cSize, "%lu", Size);
+                fclose(File);
+                Sleep(1000);
+                SafeSend(this->server, cSize, MAX_PATH);
+                Sleep(1000);
+                SafeSend(this->server,Buffer,Size);
+            }
+            free(Buffer);
+        }
+        else if(substring=="(F)"|| substring=="(f)" ){
+            send(client, buffer, sizeof(buffer), 0);
+            cout << "Mensaje enviado!" << endl;
+        }
+        else{
+            cout << "Error de comando!" << endl;
+            memset(buffer, 0, sizeof(buffer));
+            Enviar();
         }
         memset(buffer, 0, sizeof(buffer));
     }
+    int SafeSend(SOCKET s, char* buf, int buflen)
+    {
+	int sendlen = 0;
+	int totalsend = 0;
+	int remaining = buflen;
+
+	while(sendlen != buflen)
+	{
+		sendlen = send(s, &buf[totalsend], remaining, 0);
+
+		if(sendlen == SOCKET_ERROR)
+		{
+			return SOCKET_ERROR;
+		}
+
+		totalsend = totalsend + sendlen;
+		remaining = sendlen - totalsend;
+	}
+    return 0;
+    }
+
     void CerrarSocket()
     {
        cout << "Se verifico una solicitud de finalizacion de la comunicacion. Se procedera a cerrar la aplicacion" << endl;
